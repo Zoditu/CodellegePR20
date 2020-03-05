eec7c9e5c04d5ddec9fa2fb77b297c185cebe935
nayeliatzin98 <47835031+nayeliatzin98@users.noreply.github.com>
2020-03-05 11:33:13 -0600

    Ejercicios Codellege
    Ejercicios HTML, Css, Boostrap, JavaScript y JQuery.

---
+++ Nayeli Atzin/Codellege/javascript/alumnos.js
@@ -0,0 +1,122 @@
+var alumnos = [
+    {
+    nombre : 'Nayeli Atzin',
+    apellidoPaterno: 'Huerta',
+    apellidoMaterno: 'Castañeda',
+    edad: 21,
+    fechaNac: '25-08-1998',
+    sexo: 'F',
+    regalos: 'Cosas que quisiera de cumpleaños: Libros, libros y mas libros. '
+    },
+
+    {
+    nombre : 'Juan Pablo Javier',
+    apellidoPaterno: 'Garcia',
+    apellidoMaterno: 'Castillo',
+    edad: 20,
+    fechaNac: '07-06-1999',
+    sexo: 'M',
+    regalos: 'Cosas que quisiera de cumpleaños: XboxOne, MacBook Pro, un departamento, lo que sea.'
+    },
+
+    {
+    nombre: 'Erick Martin',
+    apellidoPaterno: 'Fernandez',
+    apellidoMaterno: 'Jimenez',
+    edad: 20,
+    fechaNac:'20-02-2000',
+    sexo:'M'
+    },
+
+    {
+    nombre: 'Enrique Alejandro',
+    apellidoPaterno: 'Castro',
+    apellidoMaterno: 'González',
+    edad: 21,
+    fechaNac: '07-06-1998',
+    sexo: 'M',
+    regalos: 'Cosas que quisiera de cumpleaños: Cualquier cosa de Arduino, Ropa de Marca Laxmi o estilo parecida, Ropa de Tigres'
+    },
+
+    {
+    nombre: 'Guadalupe',
+    apellidoPaterno: 'Limon',
+    apellidoMaterno: 'Flores',
+    edad: 24,
+    fechaNac: '06-10-1995',
+    sexo: 'F',
+    regalos: 'Cosas que quisiera de cumpleaños: Prisma Color Premier Coloring Kit, Colores Faber Castell, Lienzos, Caballete' 
+    },
+
+    {
+    nombre: 'Juan Pablo',
+    apellidoPaterno: 'Villanueva ',
+    apellidoMaterno: 'Pámanes',
+    edad: 21,
+    fechaNac:'14-10-1998',
+    sexo:'M',
+    regalos: 'Cosas que quisiera de Cumpleaños: Tira de luces led'
+    },
+
+    {
+    nombre : 'Dario André',
+    apellidoPaterno: 'Alfaro',
+    apellidoPaterno: 'Hernández',
+    edad: 23,
+    fechaNac: '21-02-1997',
+    sexo: 'M',
+    regalos: 'Cosas que quisiera de Cumpleaños: Airpods'
+    },
+
+    {
+    nombre:'Guadalupe',
+    apellidoPaterno: 'Hernández',
+    apellidoMaterno: 'Sánchez',
+    edad: 25,
+    fechaNac: '21-10-1994',
+    sexo:'F',
+    regalos: 'Cosas que quisiera de Cumpleaños: Algo de Avengers, Harry Potter o StarWars'
+    },
+
+    {
+    nombre: 'Kevin Fernando',
+    apellidoPaterno: 'Navarro',
+    apellidoMaterno: 'Garza',
+    edad: 23,
+    fechaNac: '13-11-1996',
+    sexo: 'M',
+    regalos:'Cosas que quisiera de Cumpleaños: Predator Helios 700, Libros de Sudoku, Algo de Lego'
+    },
+
+    {
+    nombre: 'Elisa Paola',
+    apellidoPaterno: 'Juárez',
+    apellidoMaterno: 'Avila',
+    edad: 20,
+    fechaNac: '20-05-1999',
+    sexo: 'F',
+    regalos: 'Cosas que quisiera de Cumpleaños: Boleto para Concierto '
+    },
+
+    {
+    nombre: 'Angel Aldair',
+    apellidoPaterno: 'Bárcenas',
+    apellidoMaterno: 'Mora',
+    edad: 19,
+    fechaNac: '21-07-2000',
+    sexo: 'M',
+    regalos: 'Cosas que quisiera de Cumpleaños: Un Bugatti, Unos Audifonos'
+    }
+
+];
+var maestros = [
+    {
+        nombre: 'Kevin Martin',
+        apellidoPaterno: ' Del Campo',
+        apellidoMaterno: ' Fernandez',
+        edad: 24,
+        fechaNac: '14-04-1995',
+        sexo: 'M',
+        regalos: 'Cosas que quisiera de cumpleaños: Marcadores y Borradores'
+    }
+];
