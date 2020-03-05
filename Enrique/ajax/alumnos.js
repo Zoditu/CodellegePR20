360123f766f0587cdd0dc340d96695c6670c4118
Quique98 <61840097+Quique98@users.noreply.github.com>
2020-03-05 11:27:06 -0600

    Mis  archivos
    Subi archivos de ajax

---
+++ Enrique/ajax/alumnos.js
@@ -0,0 +1,169 @@
+
+
+var JPJ =
+{
+  nombre: 'Juan Pablo Javier',
+  apellidoPaterno: 'García',
+  apellidoMaterno: 'Castillo',
+  edad: 20,
+  fechaNac: '07-06-1999',
+  sexo: 'M',
+  regalo: 'Xbox one, MacBook pro, Un departamento.',
+  Saludo: function()
+  {
+     console.log("Hola soy " + this.nombre);
+  }
+};
+
+var Enrique =
+{
+  nombre: 'Enrique Alejandro',
+  apellidoPaterno: 'Castro',
+  apellidoMaterno: 'González',
+  edad: 21,
+  fechaNac: '07-06-1998',
+  sexo: 'M',
+  regalo: 'Ropa de tigres, ropa de laxmi, cosas de arduino.',
+  Saludo: function()
+  {
+     console.log("Hola soy " + this.nombre);
+  }
+};
+
+var Erick={
+   nombre: 'Erick Martin',
+   apellidoPaterno: 'Fernandez',
+   apellidoMaterno: 'Jimenez',
+   edad: 20,
+   fechaNac:'20-02-2000',
+   sexo:'M',
+   regalo: 'Ropa.',
+  Saludo: function()
+  {
+     console.log("Hola soy " + this.nombre);
+  }
+};
+
+var Elisa = {
+ nombre: 'Elisa Paola',
+ apellidoPaterno: 'Juárez',
+ apellidoMaterno: 'Avila',
+ edad: 20,
+ fechaNac: '20-05-1999',
+ sexo: 'F',
+ regalo: 'Boleto para cualquier concierto vigente.',
+  Saludo: function()
+  {
+     console.log("Hola soy " + this.nombre);
+  }
+}
+
+var Kevin = {
+  nombre: 'Kevin Fernando',
+  apellidoPaterno: 'Navarro',
+  apellidoMaterno: 'Garza',
+  edad: 23,
+  fechaNac: '13-11-1996',
+  sexo: 'M',
+  regalo: 'Predator Helios 700, Libro de Sudokus, Algo de lego.',
+  Saludo: function()
+  {
+     console.log("Hola soy " + this.nombre);
+  }
+}
+
+var Nayeli =
+{ 
+   nombre: 'Nayeli Atzin',
+   apellidoPaterno: 'Huerta',
+   apellidoMaterno: 'Castañeda',
+   edad: 21,
+   fechaNac: '25-08-1998',
+   sexo: 'F',
+   regalo: 'Un libro, el que sea :).',
+  Saludo: function()
+  {
+     console.log("Hola soy " + this.nombre);
+  }
+}
+ 
+var Angel = {
+ nombre: 'Angel Aldair',
+ apellidoPaterno: 'Bárcenas',
+ apellidoMaterno: 'Mora',
+ edad: 19,
+ fechaNac: '21-07-2000',
+ sexo: 'M',
+ regalo: 'Un bugatti o Audífonos bose.',
+  Saludo: function()
+  {
+     console.log("Hola soy " + this.nombre);
+  }
+}
+
+var Guadalupe=
+{
+    nombre:'Guadalupe',
+    apellidoPaterno: 'Hernández',
+    apellidoMaterno: 'Sánchez',
+    edad: 25,
+    fechaNac: '21-10-1994',
+    sexo:'F',
+    regalo: 'Algo de avengers, Harry Potter o Star Wars.',
+  Saludo: function()
+  {
+     console.log("Hola soy " + this.nombre);
+  }
+};
+
+var JP = {
+  nombre: 'Juan Pablo',
+  apellidoPaterno: 'Villanueva ',
+  apellidoMaterno: 'Pámanes',
+  edad: 21,
+  fechaNac:'14-10-1998',
+  sexo:'M',
+  regalo: 'Una tira de luces led.',
+  Saludo: function()
+  {
+     console.log("Hola soy " + this.nombre);
+  }
+};
+
+var Wudu = 
+{
+  nombre: 'Guadalupe',
+  apellidoPaterno: 'Limon',
+  apellidoMaterno: 'Flores',
+  edad: 24,
+  fechaNac: '06-10-1995',
+  sexo: 'F',
+  regalo: 'Prisma color premier coloring kit, Colores faber Castell, Lienzos, Caballete.',
+   Saludo: function()
+  {
+     console.log("Hola soy " + this.nombre);
+  }
+
+}
+
+var Dario = 
+{
+  nombre: 'Darío André',
+  apellidoPaterno: 'Alfaro',
+  apellidoMaterno: 'Hernández',
+  edad: 23,
+  fechaNac: '21-02-1997',
+  sexo: 'F',
+  regalo: 'Airpods.',
+  Saludo: function()
+  {
+     console.log("Hola soy " + this.nombre);
+  }
+
+
+}
+
+var alumnos= [Enrique, Elisa, Erick, Angel, Dario, Guadalupe, Wudu, Kevin, Nayeli, JP];
+
+
+
